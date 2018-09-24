bb539a17caa6dffb9c63c74d9623e04676900527
Craig Dunlop <craigdun9393@gmail.com>
2018-09-24 18:51:29 +0100

    added simple css

--- controller.rb
+++ controller.rb
@@ -2,6 +2,7 @@ require('sinatra')
 require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative("./models/game")
+also_reload('./models/*')
 
 get "/game/:hand1/:hand2" do
   game = Game.new(params[:hand1],params[:hand2])
